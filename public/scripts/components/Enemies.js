15d29c98e671574e42c075543197261f36388e6a
roflantohan <47608077+roflantohan@users.noreply.github.com>
2020-06-23 04:52:31 +0300

    Add arrays type of enemies and maps

---
+++ public/scripts/components/Enemies.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const Enemies = {
+    enemy_img: [],
+    
+    enemyTypes: [
+        {
+            id: 0,
+            name: 'enemy_0',
+            hp: 2,
+            speed: 1.1,
+            cash: 1,
+        },
+        {
+            id: 1,
+            name: 'enemy_1',
+            hp: 5,
+            speed: 1.2,
+            cash: 1.3,
+        },
+        {
+            id: 2,
+            name: 'enemy_2',
+            hp: 7,
+            speed: 1.05,
+            cash: 1.4,
+        },
+        {
+            id: 3,
+            name: 'enemy_3',
+            hp: 10,
+            speed: 1.025,
+            cash: 1.7,
+        }
+    ],
+    loadImg : () => {
+        for(let i = 0; i < Enemies.enemyTypes.length; i++){
+            Enemies.enemy_img[i] = new Image();
+            Enemies.enemy_img[i].src = "img/" + Enemies.enemyTypes[i].name + ".png";
+            Enemies.enemyTypes[i].img = Enemies.enemy_img[i];
+        }
+    }
+}
