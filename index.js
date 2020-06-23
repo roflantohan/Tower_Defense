f2c38b25b1612f5a58924700e5bb9f1408913794
roflantohan <47608077+roflantohan@users.noreply.github.com>
2020-06-23 05:31:12 +0300

    fix error R10 Heroku

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const serve = require('koa-static');
 const koa = require('koa');
 const app = new koa();
+const PORT = process.env.PORT || 3000;
 
 app.use(serve(__dirname + '/public'));
 
-app.listen(5000);
+app.listen(PORT);
